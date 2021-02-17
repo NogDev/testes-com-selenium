d62716e0eec812849a0094ae54674ed6a0621419
304867172 <nogueira@pm.ba.gov.br>
2021-02-17 18:29:26 -0300

    Refatorando classe de testes de login

--- leilao-selenium/src/test/java/br/com/alura/leilao/login/LoginTest.java
+++ leilao-selenium/src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -8,10 +8,12 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -21,42 +23,51 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 	
+	private static final String URL_LOGIN = "http://localhost:8080/login";
+	private ChromeDriver browser;
+	
+	
+	@BeforeAll
+	public static void beforeAll() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+	}
+
+	@BeforeEach
+	public void beforeEach() {
+		this.browser = new ChromeDriver();	
+		browser.navigate().to(URL_LOGIN);
+	}
+	
+	@AfterEach
+	public void afterEach() {
+		browser.quit();
+	}
+	
+	
+	
 	@Test
 	public void deveriaEfetuarLoginComDadosValidos() {
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-
-		WebDriver browser = new ChromeDriver();	
 		
-		browser.navigate().to("http://localhost:8080/login");
+		
 		browser.findElement(By.id("username")).sendKeys("fulano");
 		browser.findElement(By.id("password")).sendKeys("pass");
 		browser.findElement(By.id("login-form")).submit();
 		
-		assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
+		assertFalse(browser.getCurrentUrl().equals(URL_LOGIN));
 		assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
-		
-		browser.quit();
 
 	}
 
 	@Test
 	public void naoDeveriaEfetuarLoginComDadosInvalidos() {
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
 		
-		WebDriver browser = new ChromeDriver();	
-		
-		browser.navigate().to("http://localhost:8080/login");
 		browser.findElement(By.id("username")).sendKeys("invalido");
 		browser.findElement(By.id("password")).sendKeys("q12312312");
 		browser.findElement(By.id("login-form")).submit();
 		
-		assertTrue(browser.getCurrentUrl().equals("http://localhost:8080/login?error"));
+		assertTrue(browser.getCurrentUrl().equals(URL_LOGIN + "?error"));
 		assertTrue(browser.getPageSource().contains("Usuário e senha inválidos."));
 		assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("usuario-logado")));
-
-		
-		browser.quit();
-		
 	}
 
 }
